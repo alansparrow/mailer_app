edf0f27eb837260fd0a1fc78ef6e62dccbbb7b98
CSIGI <trannguyenbaotrung@gmail.com>
2013-05-06 04:04:42 +0900

    Change to Gmail smtp

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ActionMailer::Base
-  default :from => "notifications@example.com"
+  default :from => "admin@sagwija.com"
  
   def welcome_email(user)
     @user = user
--- config/environment.rb
+++ config/environment.rb
@@ -5,11 +5,11 @@ require File.expand_path('../application', __FILE__)
 MailerApp::Application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-  :address        => 'smtp.sendgrid.net',
+  :address        => 'smtp.gmail.com',
   :port           => '587',
   :authentication => :plain,
-  :user_name      => ENV['SENDGRID_USERNAME'],
-  :password       => ENV['SENDGRID_PASSWORD'],
+  :user_name      => "baotrungtn@gmail.com",
+  :password       => "fpthyundai",
   :domain         => 'heroku.com',
   :enable_starttls_auto => true
 }
