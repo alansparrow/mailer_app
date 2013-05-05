1341bd1e1d544eb2eb446b2faa7d73bf77b77cf3
CSIGI <trannguyenbaotrung@gmail.com>
2013-05-06 02:55:46 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MailerApp::Application.config.secret_token = 'd3d22ec52ba3f3a36644a19a72b953fce45a19baf68ae40b82bdaab9dc670671aa53257c1b3096bfa75cd19e276ae8d26e6d9f41e578bac00e1186001a901eeb'
